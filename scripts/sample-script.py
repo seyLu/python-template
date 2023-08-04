adc1e0bcdf0355722b7339e6951dcfe53e4c12e0
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-04 10:55:34 +0800

    feat: gitignore vscode config

---
+++ scripts/sample-script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""Docstring for script."""
+
+__author__ = "seyLu"
+__github__ = "github.com/seyLu"
+
+__licence__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "seyLu"
+__status__ = "Prototype"
+
+
+def main():
+    pass
+
+
+def function(*args, **kwargs) -> None:
+    """Docstring for function."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
